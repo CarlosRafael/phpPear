51dd4269f0624b1717c97c588ad95bd535f54648
7656596 <USUARIOS7656596@ltsp323.(none)>
2016-07-07 09:11:02 -0300

    configuração inicial]

---
+++ config/config.default.php
@@ -0,0 +1,16 @@
+﻿<?php
+//################################################ EDITAR O DIRETÓRIO ########
+define('DIR', "/var/www/html/tutorial");
+define('MODEL_DIR', DIR."/model");
+define('VIEW_DIR', DIR."/view");
+define('CONTROLLER_DIR', DIR."/controller");
+
+ini_set('include_path', DIR.'/pear');
+
+require_once DIR . '/pear/DB/DataObject.php';
+require_once DIR . '/pear/Template/Sigma.php';
+
+//Da um require na classe do DataObject
+$options = &PEAR::getStaticProperty('DB_DataObject','options');
+$config = parse_ini_file('db.ini',TRUE);
+$options = $config['DB_DataObject'];
